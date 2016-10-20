927df9a5e16a633e000e7758da9a936d68f49477
AD-Aleksandrov <aleksandr.aleksandrov@emlid.com>
2016-10-20 17:33:49 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='reachled',
-    version='0.1.0',
+    version='0.1.1',
     license='BSD',
     author='Aleksandr Aleksandrov',
     author_email='aleksandr.aleksandrov@emlid.com',
